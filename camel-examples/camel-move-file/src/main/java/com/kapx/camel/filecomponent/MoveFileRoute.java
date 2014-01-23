03bd16ed3eec9e0afee4a69a28f73b82808ec2c4
ekalkur <kapil.k.kumar@ericsson.com>
2014-01-23 10:17:50 +0000

    initial version

---
+++ camel-examples/camel-move-file/src/main/java/com/kapx/camel/filecomponent/MoveFileRoute.java
@@ -0,0 +1,12 @@
+package com.kapx.camel.filecomponent;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class MoveFileRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("file://source").to("log://org.apache.camel.howto?showAll=true&multiline=true").to("file://target");
+	}
+
+}
---
+++ camel-examples/camel-move-file/src/main/java/com/kapx/camel/filecomponent/SpringMain.java
@@ -0,0 +1,16 @@
+package com.kapx.camel.filecomponent;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringMain {
+	public static void main(String[] args) throws Exception {
+		final AbstractApplicationContext springContext = new ClassPathXmlApplicationContext("META-INF/spring/move-file-context.xml");
+
+		springContext.start();
+		Thread.sleep(100000);
+		springContext.stop();
+
+		springContext.close();
+	}
+}
