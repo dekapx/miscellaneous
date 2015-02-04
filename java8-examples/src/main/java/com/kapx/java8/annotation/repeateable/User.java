aa9c7c61fa72011c48aa3e00f790be0b09672a57
ekalkur <kapil.k.kumar@ericsson.com>
2015-02-04 17:23:08 +0000

    repeatable annotation demo

--- java8-examples/src/main/java/com/kapx/java8/annotation/repeateable/User.java
+++ java8-examples/src/main/java/com/kapx/java8/annotation/repeateable/User.java
@@ -1,7 +1,9 @@
 package com.kapx.java8.annotation.repeateable;
 
 /**
- * This example demonstrate the use of repeatable annotations
+ * This example demonstrate the use of repeatable annotations. Repeating annotations are a handy new
+ * feature of Java 8, as they allow you to assign multiple versions of an annotation to a single
+ * field, method, or class.
  * 
  * @author KAPIL
  *
